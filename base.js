cb672e5b5b077b644fe4b8b6b6ef8d81a1d8e12f
arbitary <slp.mike@yandex.com>
2018-09-08 18:30:46 -0400

    added base.js

---
+++ base.js
@@ -0,0 +1,16 @@
+
+// curry :: ((a, b) -> c) -> a -> b -> c
+// e.g. curry ((x,y) => x + y) (5) (6) => 11
+
+const curry = (f) => {
+    const go = (arity, acc) => {
+      return (acc.length >= arity)
+             ? f(...acc)
+             : x => go(arity, [...acc,x])
+    }
+    // invoke helper    
+    return go(f.length, [])
+  }
+
+  // export {curry}
+
